8de15c568652ee1f6ea9ffbcec051314bfd76c63
ThanuraSilva <thanurasilva1989@gmail.com>
2024-01-08 21:31:12 +0530

    AJAX with JQuery

--- app.js
+++ app.js
@@ -25,27 +25,38 @@ $(document).ready(function(){
         console.log(itemJSON)
     
          //send the data to the backend via AJAX
-
             //AJAX - Vanila JS
             //  const http = new XMLHttpRequest();
              /////////////////////////////////////////////////
-             http.onreadystatechange = () =>{
-                 if(http.readyState == 4 && http.status == 200){
-                     console.log("Success")
-                     console.log(http.readyState)
+            //  http.onreadystatechange = () =>{
+            //      if(http.readyState == 4 && http.status == 200){
+            //          console.log("Success")
+            //          console.log(http.readyState)
 
-                 }else{
-                    console.log("Failed")
-                    console.log(http.readyState)
+            //      }else{
+            //         console.log("Failed")
+            //         console.log(http.readyState)
                     
-                 }
-             }
-             ///////////////////////////////////////////////
-             http.open("POST","http://localhost:8080/pos/item",true);
-             http.setRequestHeader("Content-Type","application/json");
-             http.send(itemJSON)
-
-             
+            //      }
+            //  }
+            //  ///////////////////////////////////////////////
+            //  http.open("POST","http://localhost:8080/pos/item",true);
+            //  http.setRequestHeader("Content-Type","application/json");
+            //  http.send(itemJSON)
+            // AJAX - JQuery
+            $.ajax({
+                url:"http://localhost:8080/pos/item",
+                type:"POST",
+                data:itemJSON,
+                headers:{"Content-Type":"application/json"},
+                success: (res) =>{
+                    console.log(JSON.stringify(res))
+                },
+                error: (err)=>{
+                    console.error(err)
+                }
+              });
+       
     });
 
 });
